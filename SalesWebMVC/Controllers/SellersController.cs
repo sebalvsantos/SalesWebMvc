45560705dae8e1819d31bca86abda5c9e647ea73
Sebastião Alves dos Santos <sebalvsantos@gmail.com>
2021-07-06 19:37:53 -0300

    Aula 267. Implementando busca simples

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Controllers
 {
@@ -79,8 +80,16 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public async Task <IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch(IntegrityException)
+            {
+                return RedirectToAction(nameof(Error), new { message = "It's not possible to delete the seller ('" + id.ToString() + "') because there are sales records pending to it, yet." });
+            }
         }
 
         public async Task<IActionResult> Edit(int? id)
